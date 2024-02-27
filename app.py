2a7bef17232e1c0bfa080a636f05aa80bba1bc46
George Grainger <george.grainger-3@student.manchester.ac.uk>
2024-02-27 18:34:17 +0000

    Create initial Paws Rescue Center pages

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
+"""Flask Application for Paws Rescue Center."""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+"""1. Add a View Function for the Home page."""
+
 
 @app.route("/")
-def hello():
-    return "Welcome to the Homepage!\n"
+def home():
+    return "Paws Rescue Center 🐾"
 
 
-@app.route("/educative")
-def learn():
-    return "Happy Learning at Educative!\n"
-
-
-@app.route("/<my_name>")
-def greatings(my_name):
-    """View function to greet the user by name."""
-    return "Welcome " + my_name + "!\n"
-
-
-@app.route("/square/<int:number>")
-def show_square(number):
-    """View that shows the square of the number passed by URL"""
-    return "Square of " + str(number) + " is: " + str(number * number) + "\n"
+@app.route("/about")
+def about():
+    return """
+    We are a non-profit organization working as an animal rescue center. 
+    We aim to help you connect with the purrfect furbaby for you! 
+    The animals you find at our website are rescue animals which have been rehabilitated. 
+    Our mission is to promote the ideology of "Adopt, don't Shop"!
+    """
 
 
 if __name__ == "__main__":
+    """ """
     app.run(debug=True, host="0.0.0.0", port=3001)
