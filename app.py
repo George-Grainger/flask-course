56860dc7c64b4b6c4ba4bfe6e35e29807f892e36
George Grainger <george.grainger-3@student.manchester.ac.uk>
2024-02-27 18:29:16 +0000

    Create example dynamic routes

--- app.py
+++ app.py
@@ -2,13 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/") 
+
+@app.route("/")
 def hello():
-    return "Hello World!\n"
+    return "Welcome to the Homepage!\n"
+
 
 @app.route("/educative")
 def learn():
     return "Happy Learning at Educative!\n"
 
+
+@app.route("/<my_name>")
+def greatings(my_name):
+    """View function to greet the user by name."""
+    return "Welcome " + my_name + "!\n"
+
+
+@app.route("/square/<int:number>")
+def show_square(number):
+    """View that shows the square of the number passed by URL"""
+    return "Square of " + str(number) + " is: " + str(number * number) + "\n"
+
+
 if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = 3001)
+    app.run(debug=True, host="0.0.0.0", port=3001)
