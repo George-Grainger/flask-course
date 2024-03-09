6622423b488c371deacf0afba64d487902e4d876
George Grainger <george.grainger-3@student.manchester.ac.uk>
2024-03-09 15:11:30 +0000

    Complete dynamic templates

--- app.py
+++ app.py
@@ -1,20 +1,52 @@
 """Flask Application for Paws Rescue Center."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
-"""1. Add a View Function for the Home page."""
+PETS = [
+    {
+        "id": 1,
+        "name": "Nelly",
+        "age": "5 weeks",
+        "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles.",
+    },
+    {
+        "id": 2,
+        "name": "Yuki",
+        "age": "8 months",
+        "bio": "I am a handsome gentle-cat. I like to dress up in bow ties.",
+    },
+    {
+        "id": 3,
+        "name": "Basker",
+        "age": "1 year",
+        "bio": "I love barking. But, I love my friends more.",
+    },
+    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
+]
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    """Information regarding the Pets in the System."""
+
+    return render_template("home.html", pets=PETS)
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/details/<int:id>")
+def details(id):
+    pet = next((p for p in PETS if p.get("id") == id), None)
+
+    if pet:
+        return render_template("details.html", pet=pet)
+    else:
+        abort(404, description="No Pet was found with the given ID")
 
 
 if __name__ == "__main__":
