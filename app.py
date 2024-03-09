6ff68fd67a1e2d7d90b2045e233105e04491d6d7
George Grainger <georgegrainger1008@gmail.com>
2024-03-09 16:58:29 +0000

    Added a  bare bones auth system

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 """Flask Application for Paws Rescue Center."""
 
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, session, redirect, url_for
+from forms import LoginForm, SignUpForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "dfewfew123213rwdsgert34tgfd1234trgf"
+
+"""Information regarding the Users in the System."""
+users = [
+    {
+        "id": 1,
+        "full_name": "Pet Rescue Team",
+        "email": "team@pawsrescue.co",
+        "password": "adminpass",
+    },
+]
 
 PETS = [
     {
@@ -37,6 +50,54 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        password = form.password.data
+        # Get the first user
+        user = next(user for user in users if user["email"] == form.email.data)
+        if user and user["password"] == password:
+            message = "Successfully Logged In"
+            session["user"] = user
+        else:
+            message = "Wrong credentials. Please try again."
+        print(session)
+        return render_template("login.html", form=form, message=message)
+    print(session)
+    return render_template("login.html", form=form)
+
+
+@app.route("/logout")
+def logout():
+    print("here")
+    if "user" in session:
+        session.pop("user")
+    return redirect(url_for("home", _external=True))
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        if any(user["email"] == form.email.data for user in users):
+            message = "User already exists"
+        else:
+            message = "Successfully Created User"
+            users.append(
+                {
+                    "id": len(users) + 1,
+                    "full_name": form.full_name.data,
+                    "email": form.email.data,
+                    "password": form.password.data,
+                }
+            )
+        return render_template("signup.html", form=form, message=message)
+    elif form.errors:
+        print(form.errors.items())
+    return render_template("signup.html", form=form)
 
 
 @app.route("/details/<int:id>")
